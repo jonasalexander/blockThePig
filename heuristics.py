39953315564ec2adfeb62c0e7b7afaf89428019a
Jonas Nelle <jonas@nelle.de>
2018-12-09 21:57:47 -0500

    Add sumPigDistanceToEdge heuristic

---
+++ heuristics.py
@@ -0,0 +1,16 @@
+from util import *
+
+def sumPigDistanceToEdge(GS):
+	# calculate the sum of all pigs' distances to the edge
+	# stoneAgent will want to maximize this
+	
+	s = 0
+	for pigId in range(GS.numPigs):
+		if GS.isCaptured(pigId):
+			s += GS.cols+GS.rows
+			continue
+		if GS.isEscaped(pigId):
+			continue
+		s += len(BFSPathToEdge(GS, pigId))
+	
+	return s
